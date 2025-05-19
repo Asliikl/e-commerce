663dd80a19f1faef06007497503870a28c207743
Asliikl <asliikiel9@gmail.com>
2025-05-19 17:36:47 +0300

    Added Category CRUD operations and created Product model with category relationship

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
@@ -82,17 +84,6 @@ class AdminController extends Controller
         return redirect()->route('admin.brands')->with('status','Brand has been updated succesfuly!');
     }
     
-    public function brand_delete($id)
-    {
-        $brand = Brand::find($id);
-        if (File::exists(public_path('uploads/brands/'.$brand->image)))
-        {
-            File::delete(public_path('uploads/brands/'.$brand->image));
-        }
-        $brand->delete();
-        return redirect()->route('admin.brands')->with('status','Brand has been deleted succesfuly!');
-    }
-
     public function GenerateBrandThumbnailsImage($image,$imageName)
     {
         $destinationPath = public_path('uploads/brands');
@@ -103,4 +94,102 @@ class AdminController extends Controller
         })->save($destinationPath.'/'.$imageName);
 
     }
+    public function brand_delete($id)
+    {
+        $brand = Brand::find($id);
+        if (File::exists(public_path('uploads/brands/'.$brand->image)))
+        {
+            File::delete(public_path('uploads/brands/'.$brand->image));
+        }
+        $brand->delete();
+        return redirect()->route('admin.brands')->with('status','Brand has been deleted succesfuly!');
+    }
+    public function categories()
+    {
+        $categories=Category::orderBy('id','DESC')->paginate(10);
+        return view('admin.categories',compact('categories'));  
+    }
+    public function category_add()
+    {
+        return view('admin.category_add');
+    }
+    public function category_store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'slug' => 'required|unique:categories,slug',
+            'image' => 'nullable|mimes:png,jpg,jpeg|max:2048'
+        ]);
+
+        $category = new Category();
+        $category->name = $request->name;
+        $category->slug = Str::slug($request->name);
+        $image = $request->file('image') ;
+        $file_extention = $request->file('image')->extension();
+        $file_name = Carbon::now()->timestamp.'.'.$file_extention ;
+        $this->GenerateCategoryThumbnailsImage($image,$file_name);
+         $category->image = $file_name;
+         $category->save();
+
+         return redirect()->route('admin.categories')->with('status','Category has been added succesfuly!');
+
+    }
+     public function GenerateCategoryThumbnailsImage($image,$imageName)
+    {
+        $destinationPath = public_path('uploads/categories');
+        $img = Image::read($image->path());
+        $img->cover(124,124,"top");
+        $img->resize(124,124,function ($constraint){
+            $constraint->aspectRatio();
+        })->save($destinationPath.'/'.$imageName);
+
+    }
+    public function category_edit($id)
+    {
+        $category = Category::findOrFail($id);
+        return view('admin.category_edit',compact('category'));
+    }
+    public function category_update(Request $request)
+    {
+        $request->validate([
+        'name' => 'required',
+        'slug' => 'required|unique:categories,slug,'.$request->id,
+        'image' => 'nullable|mimes:png,jpg,jpeg|max:2048'
+        ]);
+
+        $category = Category::find($request->id);
+        $category->name = $request->name;
+        $category->slug = Str::slug($request->name);
+
+        if ($request->hasFile('image')) {
+            if (File::exists(public_path('uploads/categories/'.$category->image)))
+             {
+                File::delete(public_path('uploads/categories/'.$category->image));
+            }
+            $image = $request->file('image') ;
+            $file_extention = $request->file('image')->extension();
+            $file_name = Carbon::now()->timestamp.'.'.$file_extention ;
+            $this->GenerateCategoryThumbnailsImage($image,$file_name);
+            $category->image = $file_name;
+        }
+        $category->save();
+        return redirect()->route('admin.categories')->with('status','Category has been updated succesfuly!');
+    }
+    public function category_delete($id)
+    {
+        $category = Category::find($id);
+        if (File::exists(public_path('uploads/categories/'.$category->image)))
+        {
+            File::delete(public_path('uploads/categories/'.$category->image));
+        }
+        $category->delete();
+        return redirect()->route('admin.categories')->with('status','Category has been deleted succesfuly!');
+    }
+
+    public function products()
+    {
+        $products = Product::orderBy('created_at','DESC')->paginate(10);
+        return view('admin.products',compact('products'));
+    }
+
 }
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Brand extends Model
+class Category extends Model
 {
     use HasFactory;
+    
+    //hasmany:kategori (Category) nesnesinin birden fazla ürünü (Product) olabilir
     public function products()
     {
         return $this->hasMany(Product::class);
